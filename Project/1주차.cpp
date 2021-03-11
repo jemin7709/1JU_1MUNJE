6c24d3701dae4ac6504864afa7cbf7d3656d6708
jemin7709 <jemin7709@naver.com>
2021-03-11 17:35:39 +0900

    Add project files.

---
+++ Project/1주차.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    vector<int> temp;
+    //vector<int> temp;
+    int length = progresses.size();
+
+    //작업 완료일 구하기
+    for (int i = 0; i < length; i++) {
+        int sum = progresses[i];
+        int count = 0;
+        while (sum < 100) {
+            sum += speeds[i];
+            count++;
+        }
+        temp.push_back(count);
+    }
+    //작업 우선순위 구하기
+    int count = 1;
+    for (int i = 1; i < length; i++) {
+        if (temp[0] - temp[i] > 0) {
+            count++;
+        }
+        if (temp[0] - temp[i] < 0) {
+            answer.push_back(count);
+            count = 1;
+        }
+        else if (i == length - 1) {
+            answer.push_back(count);
+        }
+    }
+    return answer;
+}
+
+int main() {
+    vector<int> A = { 96,99,98,97 };
+    vector<int> B = { 1,1,1,1 };
+    vector<int> C = solution(A, B);
+    for (int elem : C) {
+        cout << elem << ' ';
+    }
+
+}
---
+++ Project/Note.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<int> A = { 1, 2, 3 };
+	cout << A[1];
+
+}
